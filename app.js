59370909c7d700a24d541f1016b26d8dbec94448
hghuy <huyccn11@gmail.com>
2023-02-09 16:08:22 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -8,8 +9,21 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 
+// handle 404 response
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
+
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contact book application." });
+  res.json({
+    message: "Welcome to contact book application.",
+  });
 });
 
 module.exports = app;
